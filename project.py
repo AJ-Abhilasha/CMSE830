cea31f3780bf23f934e233680b0cb441218c51aa
Abhilasha Jagtap <145784286+AJ-Abhilasha@users.noreply.github.com>
2023-10-17 20:54:42 -0400

    Update project.py

--- project.py
+++ project.py
@@ -1,6 +1,12 @@
-import streamlit as st
-import seaborn as sns
+import numpy as np
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import seaborn as sns
+import altair as alt
+import streamlit as st
+import plotly.express as px
 
 col1, col2 = st.columns([1, 2])
 
@@ -18,10 +24,16 @@ df['streams'] = df['streams'].astype(int)
 scaler = StandardScaler()
 df['s_streams'] = scaler.fit_transform(df[['streams']])
 
+option = st.selectbox('select column name for showing the displot',('danceability_%', 'valence_%', 'energy_%',
+       'acousticness_%', 'instrumentalness_%', 'liveness_%', 'speechiness_%'))
+st.write('You selected:', option)
+plt2=sns.displot(df, x=option, hue="mode", multiple="stack")
+st.pyplot(plt2.fig)
 
+option = st.selectbox('select column name for showing the scatterplot',('danceability_%', 'valence_%', 'acousticness_%', 'instrumentalness_%', 'liveness_%', 'speechiness_%'))
 
 fig = px.scatter(df,
-    x="valence_%",
+    x=option,
     y="energy_%",
     size="streams",
     color="mode",

c02b19c703b618b218b76053f2e928cae2caf1df
Abhilasha Jagtap <145784286+AJ-Abhilasha@users.noreply.github.com>
2023-10-17 20:48:14 -0400

    Update project.py

--- project.py
+++ project.py
@@ -9,9 +9,35 @@ col2.markdown(" Brief intro to the app")
 
 df = pd.read_csv('spotify-2023.csv', encoding='ISO-8859-1')
 
+# Removing incorrect data from file 
+target_value = 'BPM110KeyAModeMajorDanceability53Valence75Energy69Acousticness7Instrumentalness0Liveness17Speechiness3'
+df = df[df['streams'] != target_value]
+df['streams'] = df['streams'].astype(int)
+
+# Creating a new column with standardized values for #streams
+scaler = StandardScaler()
+df['s_streams'] = scaler.fit_transform(df[['streams']])
+
 option = st.selectbox('select column name for showing the displot',('danceability_%', 'valence_%', 'energy_%',
        'acousticness_%', 'instrumentalness_%', 'liveness_%', 'speechiness_%'))
 st.write('You selected:', option)
 plt2=sns.displot(df, x=option, hue="mode", multiple="stack")
 st.pyplot(plt2.fig)
 
+fig = px.scatter(df,
+    x="valence_%",
+    y="energy_%",
+    size="streams",
+    color="mode",
+    hover_name="track_name",
+    log_x=True,
+    size_max=60,
+)
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+with tab1:
+    # Use the Streamlit theme.
+    # This is the default. So you can also omit the theme argument.
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+with tab2:
+    # Use the native Plotly theme.
+    st.plotly_chart(fig, theme=None, use_container_width=True)
